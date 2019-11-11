a37836db7ca0779a66553645997bfaf549837c76
Adriano Santos <sleipnir@bsd.com.br>
2019-11-11 16:55:02 -0300

    Remove env macro

--- templates/rust/shopping-cart/src/main.rs
+++ templates/rust/shopping-cart/src/main.rs
@@ -3,13 +3,14 @@ extern crate log;
 extern crate log4rs;
 extern crate cloudstate;
 
+use std::env;
 use log::{info};
 use cloudstate::serveless::{CloudState, EntityService};
 
 fn main() {
 
     // CloudState depends of log4rs to print messages
-    log4rs::init_file(env!("LOG_CONFIG_FILE_PATH"), Default::default()).unwrap();
+    log4rs::init_file(env::var("LOG_CONFIG_FILE_PATH").unwrap(), Default::default()).unwrap();
     info!("Starting CloudState Server...");
 
     let service = EntityService::new()
